2f92bd9d0a05d027650afc3f708773ce6ec58530
ThePerkinrex <theperkinrex@gmail.com>
2017-05-13 12:11:30 +0200

    Implement-insertInDB

--- src/org/coderdojo/utils/Game.java
+++ src/org/coderdojo/utils/Game.java
@@ -1,14 +1,23 @@
 package org.coderdojo.utils;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+
+import org.coderdojo.bd.FabricaConexiones;
+
 public class Game extends Buscable{
 	
 	String name;
 	int minAge;
 	String imgLink;
 	String description;
-	long genreId;
+	int genreId;
 	
-	public Game(long id, String name, int minAge, String imgLink, String description, long genreId) {
+	public Game(long id, String name, int minAge, String imgLink, String description, int genreId) {
 		super(id);
 		this.name = name;
 		this.minAge = minAge;
@@ -62,5 +71,56 @@ public class Game extends Buscable{
 	public String getDisplayName() {
 		return name;
 	}
+
+	@Override
+	public boolean insertInDB() {
+		
+		
+		
+		FabricaConexiones f = FabricaConexiones.getFabrica();
+    	Connection conn=null;
+    	boolean result = false;
+		try
+		{
+			conn = f.dameConexion();
+			String queryCheck = "INSERT INTO games(game_name, min_age, img_url, game_desc, genre_id) values(?,?,?,?,?)";
+	    	PreparedStatement ps = conn.prepareStatement(queryCheck);
+	    	ps.setString(1, name);
+	    	ps.setInt(2, minAge);
+	    	ps.setString(3, imgLink);
+	    	ps.setString(4, description);
+	    	ps.setInt(5, genreId);
+	    	ps.executeQuery();
+	    	
+	    	result = true;
+//	    	while (resultSet.next()){
+//	    		//TODO Add a tester
+//	    		int id = resultSet.getInt(0);
+//	    		String name = resultSet.getString(1);
+//	    		int minAge = resultSet.getInt(2);
+//	    		String imgLink = resultSet.getString(3);
+//	    		String desc = resultSet.getString(4);
+//	    		long genreId = resultSet.getLong(5);
+//	    		
+//	    		
+//	    	}
+		}
+		catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			if (conn!=null){try {
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}}
+		}
+		
+		return result;
+	}
+	
+	
 	
 }
